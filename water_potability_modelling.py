dd5b093d75d41715da7305985a6b638fd934bdd9
djc27-star <72269857+djc27-star@users.noreply.github.com>
2021-07-17 22:09:25 +0100

    finish coding

--- water_potability_modelling.py
+++ water_potability_modelling.py
@@ -1,37 +1,58 @@
-
 # load libraries
 from pandas import read_csv
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from pandas import DataFrame
+from random import seed
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.svm import SVC
+from sklearn.ensemble import BaggingClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
 
 # read in dataset
 dataset = read_csv("water_potability.csv")
 
-# check for na 
+# check for null values 
 if dataset.isnull().values.any() != False:
     print('null values present')
 
 dataset.describe()
 dataset.isnull().sum()
-# ph 491 nas, sulfate 781 nas Trihalomethanes 162 nas
+# ph 491 null values, sulfate 781 null values Trihalomethanes 162 null values
 
-# remove nas with mean for each output
+# remove null values with mean for each output
 dataset['ph'] = dataset['ph'].fillna(dataset.groupby('Potability')
-                                                     ['ph'].transform('mean'))
+                                     ['ph'].transform('mean'))
 
 dataset['Sulfate'] = dataset['Sulfate'].fillna(dataset.groupby('Potability')
-                                                               ['Sulfate'].transform('mean'))
+                                    ['Sulfate'].transform('mean'))
 
-dataset['Trihalomethanes'] = dataset['Trihalomethanes'].fillna(dataset.groupby('Potability')
-                                                        ['Trihalomethanes'].transform('mean'))
+dataset['Trihalomethanes'] = dataset['Trihalomethanes'].fillna(
+    dataset.groupby('Potability')
+                                    ['Trihalomethanes'].transform('mean'))
 
-# assess each variable has values in correct range and examine of the data
+# assess each variable has values in correct range and visualise of the data
 dataset.head(5)
 dataset.describe()
 dataset.columns
 
 for i in dataset['ph']:
-    assert isinstance(i,float) and i > 0
+    assert isinstance(i,float) 
 sns.distplot( dataset['ph'])
 plt.show()
 sns.boxplot(x = 'Potability', y = 'ph',data =dataset)
@@ -101,17 +122,10 @@ sns.pairplot(dataset, hue = "Potability")
 plt.show()
 
 # all data has correct values 
+
 # can move onto variable selection
 
-# load libraries
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import StandardScaler
-from sklearn.naive_bayes import GaussianNB
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import StratifiedKFold
-from pandas import DataFrame
-
+# seperate data into target and explanatory variables
 x = dataset.drop('Potability',axis = 1)
 y = dataset['Potability']
 
@@ -122,6 +136,7 @@ x= st_x.fit_transform(x)
 X = DataFrame(x,columns = names)
 
 # evaluate performance of variables
+seed(41)
 model = RandomForestClassifier()
 model.fit(X,y)
 plt.bar(X.columns,model.feature_importances_)
@@ -129,9 +144,10 @@ plt.xticks(rotation=90)
 plt.show()
 variable_importance = [(X.columns[list(model.feature_importances_).index(i)],i) for i in 
 sorted(model.feature_importances_, reverse = True)]
-
 # all variables appear to be important features
 
+# evaluate number of variables needed
+seed(97)
 no_feat = []
 data = []
 for i in range(len(variable_importance)):
@@ -141,27 +157,22 @@ for i in range(len(variable_importance)):
     kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
     cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
     no_feat.append((i+1,cv_results.mean())) 
-
 # no significant drop off in accuracy when adding variables
 # we will keep all variables in the moddelling
 
-# Split-out validation dataset
+# split-out validation dataset
+seed(81)
 X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1, shuffle=True)
 
-# Spot Check Algorithms
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.svm import SVC
-
+# spot Check Algorithms
+seed(22)
 models = []
-models.append(('LR', LogisticRegression()))
-models.append(('LDA', LinearDiscriminantAnalysis()))
-models.append(('KNN', KNeighborsClassifier()))
-models.append(('CART', DecisionTreeClassifier()))
-models.append(('NB', GaussianNB()))
-models.append(('SVC', SVC()))
+models.append(('LR', LogisticRegression())) # 0.620229
+models.append(('LDA', LinearDiscriminantAnalysis())) # 0.620229
+models.append(('KNN', KNeighborsClassifier())) # 0.630534
+models.append(('CART', DecisionTreeClassifier())) # 0.734733 
+models.append(('NB', GaussianNB())) # 0.630916
+models.append(('SVC', SVC())) # 0.680153
 # evaluate each model in turn
 results = []
 names = []
@@ -171,27 +182,23 @@ for name, model in models:
 	results.append(cv_results)
 	names.append(name)
 	print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
-# Compare Algorithms
+# compare Algorithms
 plt.boxplot(results, labels=names)
 plt.title('Algorithm Comparison')
 plt.show()  
+# cart is clearly strongest model 
 
-# cart is clearly strongest model, suggests random forest will be strong
-
-# try ensemble models
-from sklearn.ensemble import BaggingClassifier
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.ensemble import ExtraTreesClassifier
+# try ensemble models based off cart
 
 # build more complex algorithms
+seed(40)
 models2 = []
-models2.append(("BC",BaggingClassifier()))
-models2.append(("RF",RandomForestClassifier()))
-models2.append(("ETC",ExtraTreesClassifier()))
-models2.append(("ABC",AdaBoostClassifier()))
-models2.append(("GBC",GradientBoostingClassifier()))
-# evaluate each complex model in turn
+models2.append(("BC",BaggingClassifier())) #  0.754962
+models2.append(("RF",RandomForestClassifier())) # 0.791603
+models2.append(("ETC",ExtraTreesClassifier())) # 0.695802
+models2.append(("ABC",AdaBoostClassifier())) # 0.747710
+models2.append(("GBC",GradientBoostingClassifier())) # 0.787405
+# evaluate each ensemble model in turn
 results2 = []
 names2 = []
 for name, model in models2:
@@ -209,52 +216,58 @@ plt.show()
 # shall optimise both and pick strongest 
 
 # optimise RF
-from sklearn.model_selection import GridSearchCV
-
+seed(21)
 rs = {'bootstrap': [True, False],
  'max_features': ['auto', 'sqrt','log2'],
  'criterion' : ['entropy', 'gini'],
- 'n_estimators': [100,300,500]}
-grid = GridSearchCV(estimator = RandomForestClassifier(), param_grid = rs,cv =10)
+ 'min_samples_leaf': [1, 2, 4],
+ 'min_samples_split': [2, 5, 10]}
+grid = GridSearchCV(estimator = RandomForestClassifier(), param_grid = rs,
+                    cv =10,scoring = 'accuracy')
 grid.fit(X_train,Y_train)
 grid.best_params_
+# 'bootstrap': True 'criterion': 'gini' 'max_features': 'log2'
+# 'min_samples_leaf': 4 'min_samples_split': 2
 
-kfold = StratifiedKFold(n_splits=9, random_state=1, shuffle=True)
+# increase estimators to 1000
+seed(6)
+kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
 cv_results = cross_val_score(RandomForestClassifier(bootstrap = True, 
-            max_features = 'sqrt', criterion = 'gini', n_estimators = 300),
+            max_features = 'log2', criterion = 'gini', n_estimators = 1000,
+            min_samples_leaf = 4, min_samples_split = 2 ),
                              X_train, Y_train, cv=kfold, 
                              scoring='accuracy')
-print("rf optimised", " ", cv_results.mean()) # 0.7927369538723866
+print("rf optimised", " ", cv_results.mean()) # 0.7946564885496185
 
 # optimise GBC
-rs = {"n_estimators":[500],
-    "max_depth":[5,7,9],
-    "learning_rate":[0.1,1,5,10],
-    "loss" : ['deviance','exponential']}
-grid = GridSearchCV(estimator = GradientBoostingClassifier(), param_grid = rs,cv =10)
+seed(69)
+rs = {"max_depth":[1,3,5,7,9],
+    "learning_rate":[0.1,1,0.01,10],
+    "loss" : ['deviance','exponential'],
+    "n_estimators":[5,50,250,500]}
+grid = GridSearchCV(estimator = GradientBoostingClassifier(), param_grid = rs,
+                    cv =10,scoring = 'accuracy')
 grid.fit(X_train,Y_train)
 grid.best_params_
 
+seed(77)
 kfold = StratifiedKFold(n_splits=9, random_state=1, shuffle=True)
-cv_results = cross_val_score(GradientBoostingClassifier(learning_rate = 1,
+cv_results = cross_val_score(GradientBoostingClassifier(learning_rate = 0.1,
                                                         loss = 'exponential',
-                                                        max_depth = 7,
+                                                        max_depth = 9,
                                                         n_estimators = 500),
                              X_train, Y_train, cv=kfold, 
                              scoring='accuracy')
-print("sgb optimised", " ", cv_results.mean())
+print("sgb optimised", " ", cv_results.mean())# 0.7866342899883361
 
 # RF appears to be our strongest model so will now validate it 
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-
-
-model = RandomForestClassifier(bootstrap = True, max_features = 'sqrt', 
-                               criterion = 'gini', n_estimators = 300)
+seed(3)
+model = RandomForestClassifier(bootstrap = True, max_features = 'log2', 
+                               criterion = 'gini', n_estimators = 1000,
+                               min_samples_leaf = 4, min_samples_split = 2)
 model.fit(X_train,Y_train)
 predictions = model.predict(X_validation)
 
-print(accuracy_score(Y_validation, predictions)) # 0.8033536585365854
+print(accuracy_score(Y_validation, predictions)) # 0.8048780487804879
 print(confusion_matrix(Y_validation, predictions))
 print(classification_report(Y_validation, predictions))
